7e9460f815c3972e3c0d72e60bf2fd7494564fff
jennyejun <aprilhan@sookmyung.ac.kr>
2025-09-25 15:51:06 +0900

    Update Week04.c

--- Week04.c
+++ Week04.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-	int a, b;
-	int c1, c2, c3;
-	float c4, c5;
+	int sec1, min, sec2;
 
-	printf("input two integers :");
-	scanf_s("%i %i", &a, &b);
+	printf("input the second :");
+	scanf_s("%i", &sec1);
 
-	c1 = a + b;
-	c2 = a - b;
-	c3 = a * b;
-	c4 = a / b;
-	c5 = a % b;
+	min = sec1 / 60;
+	sec2 = sec1 % 60;
 
-	printf("+ result is %d\n", c1);
-	printf("- result is %d\n", c2);
-	printf("* result is %d\n", c3);
-	printf("/ result is %.f\n", c4);
-	printf("%% result is %.f", c5);
+	printf("the time is %d : %d", min, sec2);
 }
